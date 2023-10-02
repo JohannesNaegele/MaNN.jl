05eeaccd2b23068f51c7a2c8e609a7a67563b8bd
Johannes Nägele <johannes.naegele@adacon-consulting.de>
2023-10-02 18:52:02 +0200

    Update contracts for layer

--- src/Contracts.jl
+++ src/Contracts.jl
@@ -1,11 +1,18 @@
-abstract type NeuronalNetwork end
 abstract type AbstractLayer end
 
-function train!(net::NeuronalNetwork, data, loss) end
+"""
+A layer is a function which produces some output/prediction given some input values.
+"""
+function (layer::AbstractLayer)(input) end
 
-function predict(net::NeuronalNetwork, data) end
+"""
+Returns the loss and the corresponding gradient.
+We need this because we don't have automatic differentiation!
+"""
+function withgradient(layer::AbstractLayer, loss(d)) end
 
-# struct MultiLayerPerceptron <: NeuronalNetwork
-#     weights
-#     activations
-# end
+"""
+    update!(layer, grad, opt)
+Perform gradient descent on the layer given a gradient with an optimizer (which chooses e.g. the learning rate).
+"""
+function update!(layer::AbstractLayer, grad, opt) end # example
