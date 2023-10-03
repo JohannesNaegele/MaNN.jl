d1be142397dd4e88c6e81e5859fd496a5a233a0b
Johannes Nägele <johannes.naegele@adacon-consulting.de>
2023-10-03 13:44:57 +0200

    Add explanations for convolutional layer

--- src/layers/Convolutional.jl
+++ src/layers/Convolutional.jl
@@ -1,1 +1,6 @@
 # TODO: write convolutional layer
+# https://en.wikipedia.org/wiki/Convolution#Discrete_convolution
+# https://en.wikipedia.org/wiki/Discrete_Fourier_transform#Circular_convolution_theorem_and_cross-correlation_theorem
+# https://en.wikipedia.org/wiki/Fast_Fourier_transform
+
+# TODO: (padding + striding)
