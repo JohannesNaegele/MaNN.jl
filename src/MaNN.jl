7141d4f130caf6e6348b5ac970df2ee0269c2b2e
Johannes Nägele <johannes.naegele@adacon-consulting.de>
2023-10-03 23:02:28 +0200

    Add includes for working MNIST example

--- src/MaNN.jl
+++ src/MaNN.jl
@@ -1,6 +1,16 @@
 module MaNN
 
+export gradient, gradient_hardcoded, train_hardcoded!
+export Chain, Dense
+export BoringOptimizer
+export cross_entropy
+
 include("Contracts.jl")
+include("Functions.jl")
+include("./layers/Dense.jl")
+include("Chain.jl")
+include("Optimizer.jl")
+include("Train.jl")
 include("Metrics.jl")
 
-end
+end # end module Mann
