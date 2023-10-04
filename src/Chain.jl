0eaaccfdad8692b5884068f7ee7036120c8b67a6
Johannes Nägele <johannes.naegele@adacon-consulting.de>
2023-10-04 03:13:45 +0200

    Add parameter return of Chain

--- src/Chain.jl
+++ src/Chain.jl
@@ -1,11 +1,23 @@
-struct Chain{T <: Vector{S}, S <: AbstractLayer}
+struct Chain{T <: AbstractVector}
     layers::T
 end
 
+Chain(args...) = Chain([args...])
+
 function (chain::Chain)(input::AbstractArray)
     output = deepcopy(input)
-    for layer in Chain.layers
+    for layer in chain.layers
         output = layer(output)
     end
     return output
+end
+
+params(x) = Float64[]
+
+"""
+This function return the parameters of our layers.
+However, it might make sense to change this implementation with the introduction of autodiff.
+"""
+function params(chain::Chain{T}) where T
+    return [params(layer) for layer in chain.layers]
 end
