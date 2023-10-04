ed90fdfaea9c0c8f5161470a229aeb63133993d6
Johannes Nägele <johannes.naegele@adacon-consulting.de>
2023-10-04 03:24:07 +0200

    BoringOptimizer works

--- src/Optimizer.jl
+++ src/Optimizer.jl
@@ -2,8 +2,13 @@ struct BoringOptimizer <: AbstractOptimizer
     learning_rate
 end
 
-function update!(opt::BoringOptimizer, params, grad) 
+"""
+The type/layout of params might get changed in the future.
+"""
+function update!(opt::BoringOptimizer, params, grad)
     for i in eachindex(params)
-        params[i] -= opt.learning_rate .* grad[i]
+        for j in eachindex(params[i])
+            params[i][j] .-= opt.learning_rate .* grad[i][j]
+        end
     end
 end
