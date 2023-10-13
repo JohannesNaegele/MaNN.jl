e2f9104e76f42c872236d5bc94f7eb726e11ccf4
Daniel Mann <119410274+DanielMann27@users.noreply.github.com>
2023-10-13 20:28:07 +0200

    Erster Vorläufiger primitive Implementierung

--- src/layers/Convolutional.jl
+++ src/layers/Convolutional.jl
@@ -3,4 +3,15 @@
 # https://en.wikipedia.org/wiki/Discrete_Fourier_transform#Circular_convolution_theorem_and_cross-correlation_theorem
 # https://en.wikipedia.org/wiki/Fast_Fourier_transform
 
+struct Convolution <: AbstractLayer
+filter
+input
+end
+
+function set_filter(conv::Convolution)(matrix)
+    conv.filter = matrix
+end
+
+function set_
+
 # TODO: (padding + striding)
