106536664cfc3cc9536ff3606d9b07ac093f6410
Daniel Mann <119410274+DanielMann27@users.noreply.github.com>
2023-10-25 19:41:30 +0200

    Anpassungen nach Gespräch

--- src/layers/Convolutional.jl
+++ src/layers/Convolutional.jl
@@ -6,84 +6,53 @@
 # Definiere einen Convolution struct mit filter, input und output 
 struct Convolution <: AbstractLayer
     filter
-    input
-    output
-end
-
-#Setter func zum Übergeben der nötigen Daten
-function set_filter(conv::Convolution)(matrix)
-    conv.filter = matrix
-end
-
-function set_input(conv::Convolution)(matrix)
-    conv.input = matrix
 end
 
 #Primitive Berechnung des Outputs durch schrittweise Summierung der Ergebnisses der Multplikation der Inputs und dem Filter
-function calc_output_prim(conv::Convolution)(padd::Int)
-    size_in = size(conv.input)
+function calc(conv::Convolution)(input, output)
+    size_in = size(input)
     size_fil = size(filter)
 
-    
-    for t in 1:size_in[4]
+    for k in 1:size_in[3]
         for i in 1:(size_in[1]-size_fil[1]+1)
             for j in 1:(size_in[2]-size_fil[2]+1)
-                for k in 1:size_in[3]
-                    conv.output[i, j, k, t] = sum(conv.input[i:i+size_fil[1]-1, j:j+size_fil[2]-1, k, t] .* conv.filter)
-                end
+                output[i, j, k] = sum(input[i:i+size_fil[1]-1, j:j+size_fil[2]-1, k] .* conv.filter)
             end
         end
     end
-
+    
+    return output
 end
 
 #Berechnung mit optinalem Padding und factor zur Befüllung
-function calc_output_pad(conv::Convolution)(padd::Int,factor::Int)
-    size_in = size(conv.input)
-    size_fil = size(filter)
-
-    if padd > 0
-
-        index = size(size_in)
+function calc_output_pad(conv::Convolution)(padd::Int, factor::Int, input, output)
+    size_in = size(x)
+    size_fil = size(y)
     
-        e_l_o = ones((floor(Int, (size_fil[1] - 1) / 2), size_in[2], size_in[3], size_in[4])) * factor
-        e_l_u = ones((floor(Int, size_fil[1] / 2), size_in[2], size_in[3], size_in[4])) * factor
-        x = [e_l_o; conv.input; e_l_u]
-        size_in = size(x)
-    
-        e_h_l = ones((size_in[1], floor(Int, (size_fil[2] - 1) / 2), size_in[3], size_in[4])) * factor
-        e_h_r = ones((size_in[1], floor(Int, size_fil[2] / 2), size_in[3], size_in[4])) * factor
-        x = [e_h_l conv.input e_h_r]
+    if size_in[1] + p < size_fil[1] || size_in[2] + p < size_fil[2]
+        error("Filter and Input have unworkable dimensions")
+    end
+
+    if p > 0
+
+        e_l = ones(p, size_in[2], size_in[3]) * w
+        input = [e_l; input; e_l]
+        size_in = size(input)
+
+        e_h = ones((size_in[1], p, size_in[3])) * w
+        input = [e_h_l input e_h_r]
         
     
-        for t in 1:index[4]
-            for i in 1:index[1]
-                for j in 1:index[2]
-                    for k in 1:index[3]
-                        conv.output[i, j, k, t] = sum(conv.input[i:i+size_fil[1]-1, j:j+size_fil[2]-1, k, t] .* conv.filter)
-                    end
-                end
-            end
-        end
-    
+        output = conv.calc_output_prim(input,output)
+            
     else
-    
-        for t in 1:size_in[4]
-            for i in 1:(size_in[1]-size_fil[1]+1)
-                for j in 1:(size_in[2]-size_fil[2]+1)
-                    for k in 1:size_in[3]
-                        conv.output[i, j, k, t] = sum(conv.input[i:i+size_fil[1]-1, j:j+size_fil[2]-1, k, t] .* conv.filter)
-                    end
-                end
-            end
-        end
+        
+        output = conv.calc_output_prim(input,output)
 
     end
+
+    return output
 end
 
-#Getter Func für Output
-function get_output(conv::Convolution)
-    return conv.output
-end
 
-# TODO: (padding + striding)
+# TODO: (striding)
