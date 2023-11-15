7bd7a46aa5c30fc53940b0918604dd5fe293dc48
Daniel Mann <119410274+DanielMann27@users.noreply.github.com>
2023-11-15 23:42:05 +0100

    Änderungen Tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,12 +5,12 @@ using Test
     # Write your tests here.
 
 
-    Convolution t([4 5; 5 6])
-    @test t.calc([1 1 1 1; 2 2 2 2; 3 3 3 3;;;],[4 5; 5 6],1) == [31 31 31; 51 51 51;;;]
-    @test t.calc([1 1 1 1; 2 2 2 2; 3 3 3 3;;;],[4 5; 5 6],2) == [31 31;;;]
-    @test t.calc_conv([1 1 1 1; 2 2 2 2; 3 3 3 3;;;],0,0,1) == [31 31 31; 51 51 51;;;]
-    @test t.calc_conv([1 1 1 1; 2 2 2 2; 3 3 3 3;;;],0,0,2) == [31 31;;;]
-    @test t.calc_conv([1 1 1 1; 2 2 2 2; 3 3 3 3;;;],1,0,1) == [6 11 11 11 5; 11 31 31 31 14; 28 51 51 51 23; 15 27 27 27 12;;;]
-    @test_throws "Stride needs to be at least 1" t.calc_conv([1 1 1 1; 2 2 2 2; 3 3 3 3;;;],0,0,0) == [31 31;;;]
-    @test_throws "Filter and Input have unworkable dimensions" @test t.calc_conv([1; 2],0,0,1) == [31 31;;;]
+    t = Convolution([4 5; 5 6])
+    @test calc(t,[1 1 1 1; 2 2 2 2; 3 3 3 3;;;],1) == [31 31 31; 51 51 51;;;]
+    @test calc(t,[1 1 1 1; 2 2 2 2; 3 3 3 3;;;],2) == [31 31;;;]
+    @test calc_conv(t,[1 1 1 1; 2 2 2 2; 3 3 3 3;;;],0,0,1) == [31 31 31; 51 51 51;;;]
+    @test calc_conv(t,[1 1 1 1; 2 2 2 2; 3 3 3 3;;;],0,0,2) == [31 31;;;]
+    @test calc_conv(t,[1 1 1 1; 2 2 2 2; 3 3 3 3;;;],1,0,1) == [6 11 11 11 5; 11 31 31 31 14; 28 51 51 51 23; 15 27 27 27 12;;;]
+    @test_throws "Stride needs to be at least 1" calc_conv(t,[1 1 1 1; 2 2 2 2; 3 3 3 3;;;],0,0,0) == [31 31;;;]
+    @test_throws "Filter and Input have unworkable dimensions" @test calc_conv(t,[1; 2],0,0,1) == [31 31;;;]
 end
